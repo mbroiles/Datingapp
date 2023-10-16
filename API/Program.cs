364ef0d7972b823b4b0338fea4c0b79e85a1ade0
Michael Broiles <michaelbroiles@yahoo.com>
2023-10-16 14:01:03 -0400

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -19,8 +19,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-//app.UseAuthorization();
-
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
